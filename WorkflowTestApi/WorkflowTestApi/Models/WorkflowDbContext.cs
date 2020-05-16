08cf54121a676488992520d5aef09fad9dc0576f
Medoune Kane <kmedtech@gmail.com>
2020-05-16 15:52:50 +0000

    Initialisation d'EntityFramework

---
+++ WorkflowTestApi/WorkflowTestApi/Models/WorkflowDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkflowTestApi.Models
+{
+    public class WorkflowDbContext : DbContext
+    {
+        public WorkflowDbContext (DbContextOptions<WorkflowDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
